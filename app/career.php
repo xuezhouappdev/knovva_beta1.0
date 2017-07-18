fa277527f7c49b8a45990f6f858d9a2fe01a6d97
Xue Zhou <xuezhouapplication@gmail.com>
2017-07-18 00:49:51 -0400

    upadated career page

---
+++ app/career.php
@@ -0,0 +1,113 @@
+<?php
+include 'header.php';
+?>
+
+
+
+<!--banner-->
+<div class="career-banner" style="">
+
+    <div class="career-slogan ">
+        <div class="container">
+            <h1>do your life’s best work here now.<br/>
+                with the whole world watching.</h1>
+            <p>Want to search for a job now? <a>Get started > </a></p>
+        </div>
+    </div>
+</div>
+
+
+<!-- department -->
+<div class="department">
+    <div class="container">
+        <p>The people here at Apple don’t just create products — they create the kind of wonder that’s revolutionized entire industries. It’s the diversity of those people and their ideas that inspires the innovation that runs through everything we do, from amazing technology to industry-leading environmental efforts. Join Apple, and help us leave the world better than we found it.</p>
+        <p>Where do you see yourself at Knovva? <a>Get started > </a></p>
+
+        <div class="row">
+
+
+                <div class="col-md-4"  >
+                    <div style="background: url('img/dpt-academic.jpg') no-repeat center center;background-size: cover">
+                    </div>
+                    <h4>Academic Operations</h4>
+                </div>
+
+
+
+                <div class="col-md-4" >
+                    <div style="background: url('img/dpt-market.jpg') no-repeat bottom left;background-size:cover;">
+                    </div>
+                    <h4>Marketing</h4>
+                </div>
+
+
+
+                <div class="col-md-4" >
+                    <div style="background: url('img/dpt-bd.jpg') no-repeat center center;background-size:cover;">
+                    </div>
+                    <h4>Business Development</h4>
+                </div>
+
+
+
+        </div>
+
+        <div class="row">
+
+
+                <div class="col-md-4" >
+                    <div style="background: url('img/dpt-marketing.jpg') no-repeat center center;background-size:cover;"></div>
+                    <h4>Product</h4>
+                </div>
+
+
+
+                <div class="col-md-4" >
+                    <div style="background: url('img/dpt-technology.jpg') no-repeat center center;background-size:cover;" ></div>
+                    <h4>Info Tech</h4>
+                </div>
+
+
+
+                <div class="col-md-4">
+                    <div style="background: url('img/dpt-executive.jpg') no-repeat center center;background-size:cover;"></div>
+                    <h4>Executive</h4>
+                </div>
+
+        </div>
+
+
+
+        <p>at Knovva Academy, we believe .... <a href="#">Learn more > </a></p>
+    </div>
+
+
+</div>
+
+<hr>
+
+<!-- weloveour people -->
+<div class="loveourpeople ">
+    <div class="container">
+        <h3>We Love Our People</h3>
+
+        <div class="row">
+            <div class="col-md-4">
+                <img class="img img-responsive" src="img/1.jpg">
+            </div>
+            <div class="col-md-4">
+                <img class="img img-responsive" src="img/3.jpg">
+            </div>
+            <div class="col-md-4">
+                <img class="img img-responsive" src="img/2.jpg">
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+<?php
+include 'footer.php';
+?>
