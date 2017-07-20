76b2f386856428205a849b8ba4a1cca83ab77e16
Xue Zhou <xuezhouapplication@gmail.com>
2017-07-20 18:25:16 -0400

    animation gif

--- app/career.php
+++ app/career.php
@@ -11,7 +11,7 @@ include 'header.php';
         <div class="container">
             <h1 class="animated fadeInDown">do your life’s best work here now.<br/>
                 with the whole world watching.</h1>
-            <p>Want to search for a job now? <a >Get started  </a></p>
+            <p>want to search for a job now? <a >get started  </a></p>
 
         </div>
     </div>
@@ -21,9 +21,9 @@ include 'header.php';
 <!-- department -->
 <div class="department">
     <div class="container">
-        <p>Knovva Academy is hiring! <br/>We are seeking energetic and enthusiastic individuals to join our Boston-based team. We are looking to add talent to our company to help us fulfill our mission of adapting education to the twenty first century. If you’re passionate about education, globalization, world cultures, or just making a difference, click on the link below to apply for one of our open positions.
+        <p>Knovva Academy is hiring! <br/>we are seeking energetic and enthusiastic individuals to join our Boston-based team. We are looking to add talent to our company to help us fulfill our mission of adapting education to the twenty first century. if you’re passionate about education, globalization, world cultures, or just making a difference, click on the link below to apply for one of our open positions.
         </p>
-        <p>Where do you see yourself at Knovva? <a>Get started  </a></p>
+        <p>where do you see yourself at Knovva? <a>get started  </a></p>
 
         <div class="row">
 
@@ -31,17 +31,17 @@ include 'header.php';
                 <div class="col-md-4" id="test" >
                     <div style="background: url('img/dpt-academic.jpg') no-repeat center center;background-size: cover">
                     </div>
-                    <h4>Academic Operations</h4>
+                    <h4>academic operations</h4>
                 </div>
                 <div id="test" class="col-md-4" >
                     <div style="background: url('img/dpt-market.jpg') no-repeat bottom left;background-size:cover;">
                     </div>
-                    <h4>Marketing</h4>
+                    <h4>marketing</h4>
                 </div>
                 <div class="col-md-4" >
                     <div style="background: url('img/dpt-bd.jpg') no-repeat center center;background-size:cover;">
                     </div>
-                    <h4>Business Development</h4>
+                    <h4>business development</h4>
                 </div>
 
             <script>
@@ -54,22 +54,22 @@ include 'header.php';
 
                 <div class="col-md-4" >
                     <div style="background: url('img/dpt-marketing.jpg') no-repeat center center;background-size:cover;"></div>
-                    <h4>Product</h4>
+                    <h4>product</h4>
                 </div>
 
                 <div class="col-md-4" >
                     <div style="background: url('img/dpt-technology.jpg') no-repeat center center;background-size:cover;" ></div>
-                    <h4>Info Tech</h4>
+                    <h4>info tech</h4>
                 </div>
 
 
                 <div class="col-md-4">
                     <div style="background: url('img/dpt-executive.jpg') no-repeat center center;background-size:cover;"></div>
-                    <h4>Executive</h4>
+                    <h4>executive</h4>
                 </div>
 
         </div>
-        <p>at Knovva Academy, we believe .... <a href="#">Learn more</a></p>
+        <p>at Knovva Academy, we believe .... <a href="#">learn more</a></p>
     </div>
 </div>
 <hr>
@@ -77,7 +77,7 @@ include 'header.php';
 <!-- weloveour people -->
 <div class="loveourpeople">
     <div class="container">
-        <h3>We Love Our People</h3>
+        <h3>we love our people</h3>
 
         <div class="row">
             <div class="col-md-4">
--- app/index.php
+++ app/index.php
@@ -11,7 +11,7 @@ include 'header.php';
             <ol class="carousel-indicators" style="z-index: 999">
                 <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
                 <li data-target="#carouselExampleControls" data-slide-to="1"></li>
-                <li data-target="#carouselExampleControls" data-slide-to="2"></li>
+<!--                <li data-target="#carouselExampleControls" data-slide-to="2"></li>-->
             </ol>
 
             <!--Slider -->
@@ -29,7 +29,7 @@ include 'header.php';
                                 <div class="col-sm-12 ">
 
                                     <h1 >
-                                        Model G20 Program.
+                                        Model G20 program.
                                     </h1>
 <!--                                    <a class="btn btn-cta">i'm interested</a>-->
                                 </div>
@@ -42,7 +42,7 @@ include 'header.php';
 
 
                 <!--Second Slider -->
-                <div class="carousel-item" style="background: url('img/index-sie.jpg') top center no-repeat;
+                <div class="carousel-item" style="background: url('img/index-sie.svg') center center no-repeat;
                                                          background-size: cover; height:90vh";>
                     <div class="container">
 
@@ -58,10 +58,10 @@ include 'header.php';
                 </div>
 
                 <!--Third Slider -->
-                <div class="carousel-item" style="background: url('img/pink.png') center center no-repeat;
-                                                         background-size: cover; height:90vh";>
-
-                </div>
+<!--                <div class="carousel-item" style="background: url('img/pink.png') center center no-repeat;-->
+<!--                                                         background-size: cover; height:90vh";>-->
+<!---->
+<!--                </div>-->
             </div>
 
             <!--control-->
@@ -98,7 +98,7 @@ include 'header.php';
                     <div class="container">
                         <div class="row justify-content-around">
 
-                            <div class="col-md-6">
+                            <div class="col-lg-6">
                                 <h4 >who are we</h4>
                                 <p class="animated flipInX">Knovva Academy is a team of passionate educators from around the world who believe that a great education doesn’t end in the classroom. We value curiosity and diversity and are united by our shared love of learning, so we came together to help students of all backgrounds experience and discuss fresh global perspectives.<br>
 
@@ -106,14 +106,13 @@ include 'header.php';
 
                             </div>
 
-                            <div class="col-md-5 " >
+                            <div class="col-lg-5 " >
 
-                                <div style="position: relative" class="animated fadeIn">
-                                    <img class="playgif img img-responsive"  data-swap="img/minion.gif" src="img/static.gif" style="width: 100%;height: 100%;cursor: pointer">
-
-
-                                    <a data-fancybox href="https://youtu.be/FE0rKL_HvbQ" style="position:absolute;left:50%;top:50%;display: block;font-size: 50px;transform: translate(-50%, -50%);"><i style="color: #d9d9d9;border-color: #d0d0d0" class="fa fa-play-circle" aria-hidden="true"></i></a>
-                                </div>
+                                <a data-fancybox href="https://youtu.be/FE0rKL_HvbQ">
+                                    <div  class="animated fadeIn videodiv">
+                                        <img class="playgif img img-responsive"  data-swap="img/gif.gif" src="img/gif-static.gif" style="width: 100%;height: 100%;cursor: pointer">
+                                    </div>
+                                </a>
 
 
 
@@ -124,35 +123,35 @@ include 'header.php';
                     </div>
                 </div>
 
-                <div class="carousel-item">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-md-6">
-                                <h4  >what we do</h4>
-                                <p class="animated flipInX">Catering to students from around the world, Knovva Academy utilizes a mix of online and in-person courses (blend learning) for teens in order to challenge their expectations about education; we strive to provoke our students to ask, think, and learn content largely untouched in their public school system. We draw on experiential and project-based learning practices to engage students in politics, engineering, computer science, literature, and more. All of these subjects can help students uncover their own passions, and their own directions. </p>
+<!--                <div class="carousel-item">-->
+<!--                    <div class="container">-->
+<!--                        <div class="row justify-content-start">-->
+<!--                            <div class="col-md-6">-->
+<!--                                <h4  >what we do</h4>-->
+<!--                                <p class="animated flipInX">Catering to students from around the world, Knovva Academy utilizes a mix of online and in-person courses (blend learning) for teens in order to challenge their expectations about education; we strive to provoke our students to ask, think, and learn content largely untouched in their public school system. We draw on experiential and project-based learning practices to engage students in politics, engineering, computer science, literature, and more. All of these subjects can help students uncover their own passions, and their own directions. </p>-->
+<!---->
+<!--                            </div>-->
+<!---->
+<!---->
+<!---->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
 
-                            </div>
-
-
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="carousel-item">
-                    <div class="container">
-                        <div class="row justify-content-start">
-                            <div class="col-md-6">
-                                <h4  >What We Value</h4>
-                                <p class="animated flipInX">We maintain deep-seated relationships with our students and parents, both during and throughout our programs. We utilize in-person informational sessions, phone calls, digital media, and webinars to build a peer community of students and parents. Our dynamic team of native Mandarin, Spanish, and English speakers allows easy communication, and clarity of cultural expectations. Knovva Academy continues to receive high praise from parents who have felt the deep involvement of their children’s participation in our advanced learning programs.
-                                </p>
-
-                            </div>
-
-
-                        </div>
-                    </div>
-                </div>
+<!--                <div class="carousel-item">-->
+<!--                    <div class="container">-->
+<!--                        <div class="row justify-content-start">-->
+<!--                            <div class="col-md-6">-->
+<!--                                <h4  >What We Value</h4>-->
+<!--                                <p class="animated flipInX">We maintain deep-seated relationships with our students and parents, both during and throughout our programs. We utilize in-person informational sessions, phone calls, digital media, and webinars to build a peer community of students and parents. Our dynamic team of native Mandarin, Spanish, and English speakers allows easy communication, and clarity of cultural expectations. Knovva Academy continues to receive high praise from parents who have felt the deep involvement of their children’s participation in our advanced learning programs.-->
+<!--                                </p>-->
+<!---->
+<!--                            </div>-->
+<!---->
+<!---->
+<!--                        </div>-->
+<!--                    </div>-->
+<!--                </div>-->
 
 
 
@@ -183,23 +182,6 @@ include 'header.php';
     <div class="container">
         <h4>our benefits</h4>
         <div class="row ">
-<!--            <div class="col-md-3 col-sm-6 animation-item">-->
-<!---->
-<!--               <div class="animation-container">-->
-<!--                   <div class="animation-inner-wrapper" >-->
-<!--                       <img class="img img-responsive" id="a11" src="img/step3-1.svg" style="position:relative;" >-->
-<!--                       <img class="img img-responsive" id="a12" src="img/step3-2.svg" >-->
-<!--                       <img class="img img-responsive" id="a13" src="img/step3-3.svg" >-->
-<!---->
-<!--                   </div>-->
-<!--               </div>-->
-<!---->
-<!---->
-<!--                <h3>1</h3>-->
-<!--                <hr/>-->
-<!--                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </p>-->
-<!---->
-<!--            </div>-->
 
             <div class="col-md-4   animation-item">
 
@@ -214,9 +196,9 @@ include 'header.php';
                 </div>
 
 
-                <h3>1</h3>
+                <h3>interactive courses</h3>
                 <hr/>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </p>
+                <p>pursue what you love and choose from a variety of fun and engaging courses or blended learning experiences. learn beyond the school curriculum and discover new ideas and concepts and develop skills for the twenty-first century.</p>
 
             </div>
 
@@ -233,9 +215,9 @@ include 'header.php';
 
 
 
-                <h3>2</h3>
+                <h3>international student base</h3>
                 <hr/>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </p>
+                <p>you will be able to meet many students coming from all over the world, including many parts of North America, South America, and Asia. This is your chance to become a global citizen and build friendships that will last a lifetime.</p>
 
             </div>
 
@@ -250,9 +232,9 @@ include 'header.php';
 
 
 
-                <h3>3</h3>
+                <h3>student care</h3>
                 <hr/>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.  </p>
+                <p>we ensure a safe space for your ideas and always provide attentive assistance with students and parents, before, throughout, and after our programs.</p>
 
             </div>
 
@@ -263,106 +245,106 @@ include 'header.php';
 
 
 <!--Feature programs-->
-<section class="feature-programs  jumbotron">
-    <div class="container">
-        <h4>feature programs</h4>
-
-       <div class="row">
-           <a class="btn">explore the programs</a>
-       </div>
-    </div>
-</section>
+<!--<section class="feature-programs  jumbotron">-->
+<!--    <div class="container">-->
+<!--        <h4>feature programs</h4>-->
+<!---->
+<!--       <div class="row">-->
+<!--           <a class="btn">explore the programs</a>-->
+<!--       </div>-->
+<!--    </div>-->
+<!--</section>-->
 
 <!-- testimonail -->
-<section class="testimonial">
-
-  <div class="container-fluid">
-      <h4>why people love us</h4>
-
-
-          <!--Quotes slider -->
-          <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">
-
-              <div class="carousel-inner" role="listbox">
-
-                  <div class="carousel-item active">
-                      <div class="container">
-                          <div class="row justify-content-center">
-                              <div class="col-md-6">
-                                  <i class="fa fa-quote-left" aria-hidden="true"></i>
-
-                                  <div class="text-wrapper">
-                                      <p>“Seeing how much I liked this program, I predict other Knovva Academy programs would be just as good or even better.”<br/></p>
-                                  </div>
-
-                                  <div class="author-wrapper">
-                                      <h6>Student Name </h6>
-                                      <p> School Name</p>
-                                  </div>
-
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="carousel-item">
-                      <div class="container">
-                          <div class="row justify-content-center">
-                              <div class="col-md-6">
-                                  <i class="fa fa-quote-left" aria-hidden="true"></i>
-
-                                  <div class="text-wrapper">
-                                      <p>"They made learning fun and exciting! Knovva Academy showed something different, a different style of discovering new concepts." </p>
-                                  </div>
-
-                                  <div class="author-wrapper">
-                                      <h6>Student Name </h6>
-                                      <p> School Name</p>
-                                  </div>
-
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-
-                  <div class="carousel-item">
-                      <div class="container">
-                          <div class="row justify-content-center">
-                              <div class="col-md-6">
-                                  <i class="fa fa-quote-left" aria-hidden="true"></i>
-
-                                  <div class="text-wrapper">
-                                      <p>“ I had an awesome experience and got the chance to improve myself. I will take any opportunities to enrich my life with Knovva Academy. ”</p>
-                                  </div>
-
-                                  <div class="author-wrapper">
-                                      <h6>Student Name </h6>
-                                      <p> School Name</p>
-                                  </div>
-
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-
-
-              </div>
-
-              <!--CONTROLLER-->
-              <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-              </a>
-
-              <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-              </a>
-          </div>  <!--Quotes slider ends-->
-
-
-  </div>
-</section>
+<!--<section class="testimonial">-->
+<!---->
+<!--  <div class="container-fluid">-->
+<!--      <h4>why people love us</h4>-->
+<!---->
+<!---->
+<!--          <!--Quotes slider -->
+<!--          <div id="carouselExampleControls2" class="carousel slide" data-ride="carousel">-->
+<!---->
+<!--              <div class="carousel-inner" role="listbox">-->
+<!---->
+<!--                  <div class="carousel-item active">-->
+<!--                      <div class="container">-->
+<!--                          <div class="row justify-content-center">-->
+<!--                              <div class="col-md-6">-->
+<!--                                  <i class="fa fa-quote-left" aria-hidden="true"></i>-->
+<!---->
+<!--                                  <div class="text-wrapper">-->
+<!--                                      <p>“Seeing how much I liked this program, I predict other Knovva Academy programs would be just as good or even better.”<br/></p>-->
+<!--                                  </div>-->
+<!---->
+<!--                                  <div class="author-wrapper">-->
+<!--                                      <h6>Student Name </h6>-->
+<!--                                      <p> School Name</p>-->
+<!--                                  </div>-->
+<!---->
+<!--                              </div>-->
+<!--                          </div>-->
+<!--                      </div>-->
+<!--                  </div>-->
+<!---->
+<!--                  <div class="carousel-item">-->
+<!--                      <div class="container">-->
+<!--                          <div class="row justify-content-center">-->
+<!--                              <div class="col-md-6">-->
+<!--                                  <i class="fa fa-quote-left" aria-hidden="true"></i>-->
+<!---->
+<!--                                  <div class="text-wrapper">-->
+<!--                                      <p>"They made learning fun and exciting! Knovva Academy showed something different, a different style of discovering new concepts." </p>-->
+<!--                                  </div>-->
+<!---->
+<!--                                  <div class="author-wrapper">-->
+<!--                                      <h6>Student Name </h6>-->
+<!--                                      <p> School Name</p>-->
+<!--                                  </div>-->
+<!---->
+<!--                              </div>-->
+<!--                          </div>-->
+<!--                      </div>-->
+<!--                  </div>-->
+<!---->
+<!--                  <div class="carousel-item">-->
+<!--                      <div class="container">-->
+<!--                          <div class="row justify-content-center">-->
+<!--                              <div class="col-md-6">-->
+<!--                                  <i class="fa fa-quote-left" aria-hidden="true"></i>-->
+<!---->
+<!--                                  <div class="text-wrapper">-->
+<!--                                      <p>“ I had an awesome experience and got the chance to improve myself. I will take any opportunities to enrich my life with Knovva Academy. ”</p>-->
+<!--                                  </div>-->
+<!---->
+<!--                                  <div class="author-wrapper">-->
+<!--                                      <h6>Student Name </h6>-->
+<!--                                      <p> School Name</p>-->
+<!--                                  </div>-->
+<!---->
+<!--                              </div>-->
+<!--                          </div>-->
+<!--                      </div>-->
+<!--                  </div>-->
+<!---->
+<!---->
+<!--              </div>-->
+<!---->
+<!--              <!--CONTROLLER-->
+<!--              <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">-->
+<!--                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
+<!--                  <span class="sr-only">Previous</span>-->
+<!--              </a>-->
+<!---->
+<!--              <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">-->
+<!--                  <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
+<!--                  <span class="sr-only">Next</span>-->
+<!--              </a>-->
+<!--          </div>  <!--Quotes slider ends-->
+<!---->
+<!---->
+<!--  </div>-->
+<!--</section>-->
 
 
 <?php
