3b79d371ab597bb3744366d3350a18dd72d056d4
Xue Zhou <xuezhouapplication@gmail.com>
2017-07-24 21:29:13 -0400

    updated programs page

---
+++ app/programs.php
@@ -0,0 +1,140 @@
+<?php
+include 'header.php';
+?>
+
+
+
+<!--banner-->
+<div class="program-banner" style="">
+
+    <div class="program-slogan ">
+        <div class="container">
+            <h1 class="animated fadeInDown">featured programs</h1>
+
+
+
+        </div>
+    </div>
+</div>
+
+
+<!-- programs -->
+<div class="programs">
+    <div class="container">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+        <div class="row ">
+            <div class="col-md-6 wow slideInLeft">
+                <!--card-->
+                <div class="shadow" >
+
+                    <!--IMG and deco-->
+                    <div class="p-image-wrapper" style="background:url('img/3.jpg') center center no-repeat;background-size:cover;">
+                        <div class="p-decoration-text" >
+
+                            <h4>Model G20</h4>
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i>07-24-2017&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-map-marker" aria-hidden="true"></i>boston&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-clock-o" aria-hidden="true"></i>2 day</h6>
+
+
+                        </div>
+                    </div>
+
+                    <!--PARA DES-->
+                    <div class="p-text-wrapper">
+                       <h6>political science, international affairs </h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula. Nullam augue augue.</p>
+                    </div>
+
+                </div>
+                <!--card-->
+            </div>
+
+
+
+            <div class="col-md-6 wow slideInRight">
+                <!--card-->
+                <div class="shadow" >
+
+                    <!--IMG and deco-->
+                    <div class="p-image-wrapper" style="background:url('img/3.jpg') center center no-repeat;background-size:cover;">
+                        <div class="p-decoration-text" >
+                            <h4>social impact and entrepreneurship</h4>
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i>07-24-2017&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-map-marker" aria-hidden="true"></i>boston&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-clock-o" aria-hidden="true"></i>2 day</h6>
+                        </div>
+                    </div>
+
+                    <!--PARA DES-->
+                    <div class="p-text-wrapper">
+                        <h6>business, social science </h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula. Nullam augue augue.</p>
+                    </div>
+
+                </div>
+                <!--card-->
+            </div>
+
+        </div>
+
+        <div class="row ">
+            <div class="col-md-6 wow slideInLeft">
+                <!--card-->
+                <div class="shadow" >
+
+                    <!--IMG and deco-->
+                    <div class="p-image-wrapper" style="background:url('img/3.jpg') center center no-repeat;background-size:cover;">
+                        <div class="p-decoration-text" >
+
+                            <h4>the harry potter reading group</h4>
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i>07-24-2017&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-map-marker" aria-hidden="true"></i>boston&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-clock-o" aria-hidden="true"></i>2 day</h6>
+
+
+                        </div>
+                    </div>
+
+                    <!--PARA DES-->
+                    <div class="p-text-wrapper">
+                        <h6>literature, fiction</h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula. Nullam augue augue.</p>
+                    </div>
+
+                </div>
+                <!--card-->
+            </div>
+
+
+
+            <div class="col-md-6 wow slideInRight">
+                <!--card-->
+                <div class="shadow" >
+
+                    <!--IMG and deco-->
+                    <div class="p-image-wrapper" style="background:url('img/3.jpg') center center no-repeat;background-size:cover;">
+                        <div class="p-decoration-text" >
+                            <h4>social impact and entrepreneurship</h4>
+                            <h6><i class="fa fa-calendar" aria-hidden="true"></i>07-24-2017&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-map-marker" aria-hidden="true"></i>boston&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-clock-o" aria-hidden="true"></i>2 day</h6>
+                        </div>
+                    </div>
+
+                    <!--PARA DES-->
+                    <div class="p-text-wrapper">
+                        <h6>business, social science </h6>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean egestas magna at porttitor vehicula. Nullam augue augue.</p>
+                    </div>
+
+                </div>
+                <!--card-->
+            </div>
+
+        </div>
+
+
+
+    </div>
+</div>
+
+
+
+
+<?php
+include 'footer.php';
+?>
