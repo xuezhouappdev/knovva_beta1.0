9b69cbfb5bf63c83a17866ada0684b49434ed122
xuezhouappdev <xuezhouapplication@gmail.com>
2017-10-18 10:24:22 -0400

    modified SEM academic JD on career

--- app/position_it_1.php
+++ app/position_it_1.php
@@ -44,18 +44,18 @@ include 'header.php';
 <!--                <p>We are looking for an Online Learning Specialist position to guide the development for an online learning course in robotics for high school students based in the United States and China. The Online Learning Specialist will be responsible for providing creative input, guidance and development of learning design and will maintain a professional-level of thoroughness and resolve to develop clear, comprehensive and usable educational programs. The OLS should feel comfortable conducting academic research on a range of subjects, collaborating with external partners, exploring trends in international education, ed-tech and be prepared to apply their learning to innovative learning opportunities for high school students in online forms.</p>-->
 
 
-                <h5>Job Description</h5>
+                <h5>Job Duties</h5>
                 <ul>
-                    <li>Define Knovva’s online marketing campaign strategy and perform digital marketing analysis to execute strategies</li>
-                    <li>Be highly specialized in search-result ranking algorithms of major search engines;
+                    <li>Define Knovva’s online marketing campaign strategy and perform digital marketing analysis to execute strategies including but not limited to: developing marketing dashboards, reports, and visualization that track KPI (key performance indicators) including Wechat followers, official promotion article page views and registration users etc.</li>
+                    <li>Be highly specialized in search-result ranking algorithms of major search engines, e.g. Google and Baidu; exploit source code regarding how to implement SEM analysis results into web content production and web source code, so as to identify and implement opportunities for optimization, including but not limited to advertising creative and landing page optimization, and optimizing the data in the system to obtain higher rankings.
                     </li>
-                    <li>By analyzing Chinese consumer characteristics, determine analytical metrics according to project purpose and target customers;
+                    <li>By analyzing Chinese consumer characteristics, determine analytical metrics according to project purpose and target customers; develop and define Knovva’s own search-engine rules and keyword rules aiming at generating better search results, attracting and retaining more users, and eventually achieving our business goal of generating web traffic.
                     </li>
-                    <li>Analyze browser data and user behavior by developing queries to extract data from Knovva’s multiple data sources including web database, SQL server database;
+                    <li>Analyze browser data and user behavior by developing queries to extract data from Knovva’s multiple data sources including web database, SQL server database and data from media campaign, in order to propose, coordinate, and conduct ad hoc analysis.
                     </li>
-                    <li>Perform data mining, data migration, data reconciliation and statistical analysis;
+                    <li>Perform data mining, data migration, data reconciliation and statistical analysis to produce insights that drive data-driven Chinese oversea study educational market.
                     </li>
-                    <li>Improve marketing intelligence wise.
+                    <li>Improve marketing intelligence wise: to analyze web site behavior using web analytics tools to determine areas of site improvement; provide ongoing Digital Marketing Intelligence primarily focused on browser with weekly, monthly, quarterly and yearly reporting as well as ad-hoc and deep-dive analysis; provide recommendations and execute strategies for keyword opportunities, campaign structuring, targeting, display network, and other facets of paid search in accordance with client goals.
                     </li>
                 </ul>
 
